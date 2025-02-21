7b881f695966c98aa55363b76714eedbae1f1b55
M-Ahmad-Usman <developer.ahmad@proton.me>
2025-02-21 19:14:35 +0500

    Add a button to generate grid dynamically of given size

--- script.js
+++ script.js
@@ -7,35 +7,66 @@ function addHoverEffect(element) {
     if (!(element instanceof HTMLElement))
         return "Element is not an HTML element";
 
-    element.addEventListener(`mouseenter`, ()=> {
+    element.addEventListener(`mouseenter`, () => {
         element.style.backgroundColor = `${getRandomColor()}`;
     });
 }
 
-let container = document.querySelector("#container");
-const gridSize = 16;
+// Returns an integer for grid size. If user cancels then returns null or if user enters empty string then returns that string
+function getGridSize() {
 
-for (let i = 0; i < gridSize; i++) {
+    let size;
 
-    // Creating a div row which will contain 16 divs
-    let rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "row-div");
+    while (!(size > 0 && size <= 50 && size != NaN)) {
+        size = prompt("Enter Grid Size of less than 51: ", 12);
 
-    for (let j = 0; j < gridSize; j++) {
-        // Creatinng column divs
-        let columnDiv = document.createElement("div");
-        columnDiv.setAttribute("class", "column-div");
-
-        // Setting the size of divs
-        columnDiv.style.cssText = `width: ${container.offsetWidth / gridSize}px; height: ${container.offsetHeight / gridSize}px;`;
-        
-        // Adding hover effect on div
-        addHoverEffect(columnDiv);
-
-        // Appending the divs to their respective row
-        rowDiv.appendChild(columnDiv);
+        if (size)
+            size = parseInt(size);
+        else
+            return size;
     }
 
-    // Appending rows each containing 16 divs to the container
-    container.appendChild(rowDiv);
+    return size;
 }
+
+function generateGrid(gridSize = 12) {
+    let container = document.querySelector("#container");
+    container.textContent = "";
+
+    for (let i = 0; i < gridSize; i++) {
+
+        // Creating a div row which will contain 16 divs
+        let rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class", "row-div");
+
+        for (let j = 0; j < gridSize; j++) {
+            // Creatinng column divs
+            let columnDiv = document.createElement("div");
+            columnDiv.setAttribute("class", "column-div");
+
+            // Setting the size of divs
+            columnDiv.style.cssText = `width: ${container.offsetWidth / gridSize}px; height: ${container.offsetHeight / gridSize}px;`;
+
+            // Adding hover effect on div
+            addHoverEffect(columnDiv);
+
+            // Appending the divs to their respective row
+            rowDiv.appendChild(columnDiv);
+        }
+
+        // Appending rows each containing 16 divs to the container
+        container.appendChild(rowDiv);
+    }
+}
+
+let gridSizeBtn = document.querySelector("#grid-size-btn");
+
+// Generates a new grid if user has given a valid size. else do nothing
+gridSizeBtn.addEventListener("click", ()=>{
+    let size = getGridSize();
+    if (size) {
+        generateGrid(size);
+    } 
+});
+
+generateGrid();
